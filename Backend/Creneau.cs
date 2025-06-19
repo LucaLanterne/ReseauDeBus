d9b995b38a018acf6f882f2c408a9df4504840f2
Luca Lanterne <83977490+LucaLanterne@users.noreply.github.com>
2025-06-19 16:03:39 +0200

    Arret, creneau, troncon

---
+++ Backend/Creneau.cs
@@ -0,0 +1,15 @@
+namespace ReseauDeBus.Backend;
+
+public class Creneau
+{
+    public (int, int) plage_creneau { get; set; }
+    public int moyenne_creneau { get; set; }
+    public int ecartType_creneau { get; set; }
+
+    public Creneau((int, int) plageCreneau, int moyenneCreneau, int ecartTypeCreneau)
+    {
+        plage_creneau = plageCreneau;
+        moyenne_creneau = moyenneCreneau;
+        ecartType_creneau = ecartTypeCreneau;
+    }
+}
---
+++ Backend/Troncon.cs
@@ -0,0 +1,21 @@
+namespace ReseauDeBus.Backend;
+
+public class Troncon
+{
+    public string id_troncon { get; }
+    public Arret arretDepart_troncon { get; set; }
+    public Arret arretArrivee_troncon { get; set; }
+    public List<Creneau> creneaux_troncon { get; }
+
+    public Troncon(string idTroncon, Arret arretDepartTroncon, Arret arretArriveeTroncon, List<Creneau> creneauxTroncon)
+    {
+        id_troncon = idTroncon;
+        arretDepart_troncon = arretDepartTroncon;
+        arretArrivee_troncon = arretArriveeTroncon;
+        creneaux_troncon = new List<Creneau>();
+        foreach (Creneau c in creneauxTroncon)
+        {
+            creneaux_troncon.Add(new Creneau(c.plage_creneau, c.moyenne_creneau, c.ecartType_creneau));
+        }
+    }
+}
